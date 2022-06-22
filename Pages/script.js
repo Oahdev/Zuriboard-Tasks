4b50415f8b826208f897a1019d6991c8e3af5321
Olamide Sodeinde <90583135+Oahdev@users.noreply.github.com>
2022-06-22 19:03:06 +0100

    Add files via upload

---
+++ Pages/script.js
@@ -0,0 +1,6 @@
+function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+}   
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block ";
+}
