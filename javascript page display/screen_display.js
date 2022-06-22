6478719468ee05ddf3beb6de37adb22f3f3536c5
Olamide Sodeinde <90583135+Oahdev@users.noreply.github.com>
2022-06-22 18:21:47 +0100

    Update screen_display.js

--- javascript page display/screen_display.js
+++ javascript page display/screen_display.js
@@ -1,1 +1,6 @@
-document.writeln("Hello World!");
+var name = "Olamide",
+    height = "186cm",
+    country = "Nigeria";
+document.writeln(name);
+document.writeln(height);
+document.writeln(country);
